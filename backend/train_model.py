a034aeab43d3dc301ff65afb20a97278ef901297
aparna25bce10363-maker <aparna.25bce10363@vitbhopal.ac.in>
2026-05-25 08:26:46 +0530

    Update train_model.py

--- backend/train_model.py
+++ backend/train_model.py
@@ -8,35 +8,20 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score
 
-# =========================
-# LOAD DATASETS
-# =========================
 
 fake = pd.read_csv("data/Fake.csv")
 real = pd.read_csv("data/True.csv")
 
-# =========================
-# ADD LABELS
-# =========================
 
 fake["label"] = 0
 real["label"] = 1
 
-# 0 = Fake
-# 1 = Real
-
-# =========================
-# COMBINE DATA
-# =========================
 
 data = pd.concat([fake, real])
 
-# Shuffle rows
+
 data = data.sample(frac=1)
 
-# =========================
-# TEXT CLEANING FUNCTION
-# =========================
 
 def clean_text(text):
 
@@ -59,16 +44,9 @@ def clean_text(text):
 # Apply cleaning
 data["text"] = data["text"].apply(clean_text)
 
-# =========================
-# FEATURES & LABELS
-# =========================
 
 x = data["text"]
 y = data["label"]
-
-# =========================
-# TRAIN TEST SPLIT
-# =========================
 
 x_train, x_test, y_train, y_test = train_test_split(
     x,
@@ -77,9 +55,6 @@ x_train, x_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
-# =========================
-# TF-IDF VECTORIZATION
-# =========================
 
 vectorizer = TfidfVectorizer(stop_words="english")
 
@@ -87,9 +62,7 @@ xv_train = vectorizer.fit_transform(x_train)
 
 xv_test = vectorizer.transform(x_test)
 
-# =========================
-# TRAIN MODEL
-# =========================
+
 
 model = SGDClassifier(
     loss="hinge",
@@ -100,23 +73,15 @@ model = SGDClassifier(
 
 model.fit(xv_train, y_train)
 
-# =========================
-# PREDICTIONS
-# =========================
 
 y_pred = model.predict(xv_test)
 
-# =========================
-# ACCURACY
-# =========================
+
 
 score = accuracy_score(y_test, y_pred)
 
 print("Accuracy:", score)
 
-# =========================
-# SAVE MODEL
-# =========================
 
 joblib.dump(model, "model.pkl")
 
